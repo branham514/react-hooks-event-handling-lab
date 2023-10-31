92c920c64192a6b6448cc7ae18fae80fd6c15b7c
branham514 <branham.gatheru@student.moringaschool.com>
2023-10-31 13:42:49 +0300

    completed

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,27 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+const EyesOnMe = () => {
+  // Event handler for the focus event
+  const handleFocus = () => {
+    console.log('Good!');
+  };
+
+  // Event handler for the blur event
+  const handleBlur = () => {
+    console.log('Hey! Eyes on me!');
+  };
+
+  return (
+    <div>
+      <button
+        onFocus={handleFocus} // Listen for the 'focus' event
+        onBlur={handleBlur} // Listen for the 'blur' event
+      >
+        Eyes on me
+      </button>
+    </div>
+  );
+};
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,39 @@
 // Code Keypad Component Here
 
-function Keypad (){
-    return (
-        <div></div>
-    )
-}
+// const Keypad =() =>{
+//     //event handler for input change
+//     const handleChange = () => {
+//         console.log("Entering password..");
+//     };
+
+//     return(
+//         <div>
+//     <input
+//     type="passsword"
+//     onchange ={handleChange} 
+//     /> 
+//     </div>
+//     );
+// };
+
+
+// export default Keypad;
+import React from 'react';
+
+const Keypad = () => {
+  // Event handler for input change
+  const handleChange = () => {
+    console.log('Entering password...');
+  };
+
+  return (
+    <div>
+      <input
+        type="password" // Use the 'password' type for a password input
+        onChange={handleChange} // Listen for the 'change' event
+      />
+    </div>
+  );
+};
 
 export default Keypad;
